ffb2ad914b8af692779e836722d26c8a770ececd
aya borhamm <114094637+aya20044@users.noreply.github.com>
2022-12-19 18:11:53 +0200

    Add files via upload

---
+++ Project 2.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Perfectnumbersapplication
+{
+    class App2
+    {
+        static void Main(string[] args)
+        { /* project 2 */
+            // start
+
+            Console.WriteLine(" Please, Enter Your Range Number from n1 to n2");
+            Console.WriteLine(" Enter n1 ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter n2");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine(" These are the peerfect numbers between your range: ");
+            int sum = 0;
+            for ( int a=x; a<=y;a++)
+            {
+                for(int k = 1; k < a; k++)
+                {
+                    if (a % k == 0)
+                    { sum = sum + k; }
+                    else continue;
+                }
+                if (sum == a) { Console.Write(a + "\t"); sum = 0; }
+                if (sum != a) sum = 0;
+            }
+            Console.ReadKey();
+            // end 
+           
+          
+
+
+
+
+
+
+        }
+
+
+    }
+}
---
+++ project number one .cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+// converion using casting operation
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using System;
+namespace Projectone
+{
+    class project1
+    {
+        static void Main(string[] args)
+        { // project one discreet task // prime numbers //start
+
+            int i = 0, j = 0;
+            Console.WriteLine("enter the first number,please");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the second number,please");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(" These are the prime numbers between your range:");
+            for (i = num1; i <= num2; i++)
+            {
+                if (i == 0 || i == 1)
+                    continue;
+                for (j = 2; j < i; j++)
+                {
+                    if (i % j == 0) break;
+                    if (i % j != 0) ;
+
+                }
+                if (i == j) Console.Write(i+ "\t");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+//End 
